f42f49a37835add1b79aac6d7358fe26a74cb134
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2018-03-18 03:35:38 +0800

    new

---
+++ latihan/lat1.py
@@ -0,0 +1,8 @@
+print("hai, semuaaa...")
+print("Hello, World")
+print("ini \t tab")
+print("Bikin Baris Baru Yuks \nBaris baru nih")
+
+print("Baris Pertama", end=' ')
+print("Lanjutan satu baris")
+print("Baris Selanjutnya")
