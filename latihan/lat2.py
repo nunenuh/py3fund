dc4940f9cfe74bcfc7dfc96daf80a2cf55a62071
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2018-03-21 07:46:17 +0800

    tambahn baru

---
+++ latihan/lat2.py
@@ -0,0 +1,10 @@
+# ini komentar, tidak akan di tampilkan dan di eksekusi
+print("Ini teks yang akan di cetak") # ini komentar di samping sintaks
+
+a = input("Masukan Nilai X = ") # input akan ber type data string
+x = int(a) # pengubahan tipe data, dari string ke integer
+y = 2*x + 3 # proses aritmatika / perhitungan
+
+# mencetak nilai Y dengan konversi ke string
+# sebelum di cetak ke layar
+print( 'Y = '+str(y) ) 
---
+++ latihan/lat3.py
@@ -0,0 +1,27 @@
+# File : lat3.py
+# tipe data dan variabel
+
+a = 'ini text' # tipe data string
+print("Tipe Data Variabel A : ",type(a))
+
+b = 10 # tipe data integer
+print("Tipe Data Variabel B : ",type(b))
+
+zz = float(b)
+print("Tipe Data Variabel ZZ : ",type(zz))
+
+c = 7.10 # tipe data float
+print("Tipe Data Variabel C : ", type(c))
+
+xx = int(c)
+print("Tipe Data Variabel XX : ", type(xx))
+
+d = [1,2,3] # tipe data list
+print("Tipe Data Variabel D : ",type(d))
+
+e = (2,3,4) # tipe data tupple
+print("Tipe Data Variabel E : ",type(e))
+
+f = {'nilai':78} # tipe data dictionary
+print("Tipe Data Variabel F : ",type(f))
+
