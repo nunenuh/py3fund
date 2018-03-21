c5435e567fffb03b35756fde93a68443468c0d93
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2018-03-21 14:04:37 +0800

    new0

---
+++ latihan/lat4.py
@@ -0,0 +1,15 @@
+# File : lat4.py
+# Lanjutan Variabel
+
+nama_saya = "Lalu Erfandi Maula Yusnu"
+lokasi_lahir = "Mataram"
+lahir_tahun = 1988
+tinggi_badan = 170
+
+# Format String Literal, hanya berlaku pada python 3.6
+# f"Ini text {variabel}"
+
+print(f"Mari membahas tentang {nama_saya}")
+print(f"Dia Lahir di {lokasi_lahir} dan di Tahun {lahir_tahun}")
+print(f"Dia memiliki tinggi badan {tinggi_badan}")
+
